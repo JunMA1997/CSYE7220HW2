2f5dbfb2bd68be78a033579625d02538b3ecfe90
JunMA <ma.jun2@husky.neu.edu>
2021-02-03 14:42:01 +0800

    gitignore,memories,500miles

---
+++ Pages/FHM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace HW2.Pages
+{
+    public class FHMModel : PageModel
+    {
+        private readonly ILogger<FHMModel> _logger;
+
+        public FHMModel(ILogger<FHMModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            // _logger.LogInformation((int)LoggingEvents.Networkking,"logwork memories", url);
+        }
+    }
+}
---
+++ Pages/Memories.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace HW2.Pages
+{
+    public class MemoriesModel : PageModel
+    {
+        private readonly ILogger<MemoriesModel> _logger;
+
+        public MemoriesModel(ILogger<MemoriesModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            // _logger.LogInformation((int)LoggingEvents.Networkking,"logwork memories", url);
+        }
+    }
+}
